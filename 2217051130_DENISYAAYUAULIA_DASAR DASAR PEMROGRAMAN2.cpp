b3f0c407275d0b94251cfc1123c51e1fc8538830
DenisyaAyuAulia <112992384+DenisyaAyuAulia@users.noreply.github.com>
2022-09-08 22:41:24 +0700

    Add files via upload

---
+++ 2217051130_DENISYAAYUAULIA_DASAR DASAR PEMROGRAMAN2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a, b;
+	
+	cin >> a >> b;	
+	cout << a + b<< end1;
+	cout << a - b<< end1
+	cout << a * b<< end1;
+	cout << a / b<< end1;
+	cout << a % b<< end1;
+		
+}
